c2a7d4d128b27d0d2c1c0ea8d565e20cfd430712
Ayushi <raiayushi0507@gmail.com>
2026-02-15 14:24:48 +0530

    add online survey code

---
+++ src/onlinesurvey/Option.java
@@ -0,0 +1,16 @@
+package onlinesurvey;
+
+import java.util.UUID;
+
+public class Option {
+    private UUID optionId;
+    private String value;
+
+    public UUID getOptionId() {
+        return optionId;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
